93637a1f5fd62c7d9699227490772eab91f788ef
Victor Areiza <victorareiza02@gmail.com>
2023-01-29 22:19:42 -0500

    Updates

--- assets/main.js
+++ assets/main.js
@@ -25,7 +25,7 @@ async function fetchData(urlApi) {
         <div
           class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
           <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
-        </div>
+        </div> 
         <div class="mt-4 flex justify-between">
           <h3 class="text-sm text-gray-700">
             <span aria-hidden="true" class="absolute inset-0"></span>
